5ac1e19fe5b8929bf7e5ea478064673a86d2feb5
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-08-30 14:13:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crystal_store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 15;
+            int goldCount;
+            int crystalCount;
+            bool enoughGold;
+
+            Console.Write("Введите количество золота: ");
+            goldCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько кристалов вы хотите купить?: ");
+            crystalCount = Convert.ToInt32(Console.Read());
+            
+            enoughGold = goldCount >= crystalPrice * crystalCount;
+            
+            goldCount *= Convert.ToInt32(enoughGold);
+            goldCount -= crystalPrice * crystalCount;
+            Console.WriteLine("Вы купили " + crystalCount +
+                 " кристаллов, у вас осталось " + (goldCount - crystalPrice * crystalCount) + " золота");
+           
+            Console.ReadKey();
+        }
+    }
+}
